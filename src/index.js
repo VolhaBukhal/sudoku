c60357ab6bfc5c5303c59f2dc27b8b7ae56d2ce9
ola <o.bukhal@gmail.com>
2021-09-01 16:08:47 +0300

    feat: add function getColumn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveSudoku(matrix) {
   // your solution
 }
+
+function getColumn (arr, index) {
+  let column = [];
+  for (let i = 0; i < arr[0].length -1; i++ ) {
+    for (let j = 0; j < arr.length; j++) {
+      if ( j == index) {
+        column.push(arr[i][j]);
+      }
+    }
+  }
+  return column;
+}
